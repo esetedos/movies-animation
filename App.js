177e4bc4d8922aa269630cc34469cde82264618c
esetedos <esther.fernandez@ikasle.aeg.eus>
2023-10-19 20:59:29 +0200

    [UPDATE] basic structure done

--- App.js
+++ App.js
@@ -1,24 +1,97 @@
+import React, {useState, useEffect} from 'react'
+import {FlatList } from 'react-native'
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { AppLoading } from 'expo';
+import {useFonts} from 'expo-font'
+import styled from 'styled-components/native';
+import Rating from './components/Rating';
+import Genre from './components/Genre';
 import { getMovies } from './api';
+import * as CONSTANTS from './constants/constants'
+
+
+const Container = styled.View`
+  flex: 1;
+`;
+
+const PosterContainer = styled.View`
+  width: ${CONSTANTS.ITEM_SIZE}px;
+`;
+
+const Poster = styled.View`
+  margin-horizontal: ${CONSTANTS.SPACING}px;
+  padding: ${CONSTANTS.SPACING * 2}px;
+  align-items: center;
+  background-color: #FFFFFF;
+  border-radius: 10px;
+`;
+
+const PosterImage = styled.Image`
+  width: 100%;
+  height: ${CONSTANTS.ITEM_SIZE * 1.2}px;
+  resize-mode: cover;
+  border-radius: 10px;
+  margin: 0 0 10px 0;
+`;
+
+const PosterTitle = styled.Text`
+  font-size: 18px;
+`;
+
+const PosterDescription = styled.Text`
+ 
+  font-size: 12px;
+`;
 
 export default function App() {
+  const [movies, setMovies] = useState([]);
+  const [loaded, setLoaded] = useState(false);
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getMovies();
+      setMovies(data);
+      setLoaded(true);
+    };
+    fetchData();
+  }, []);
+
+
+  // let [fontLoaded] = useFonts({
+  //   'Syne-Mono': require('./assets/fonts/SyneMono-Regular.ttf'),
+  // });
+
+  // if(!loaded || !fontLoaded){
+  //   return <AppLoading />
+  // }
+  // else
   return (
-    // <View style={styles.container}>
-    //   <Text>Open up App.js to start working on your app!</Text>
-    //   <StatusBar style="auto" />
-    // </View>
-    <View>
-{/* <getMovies /> */}
-    </View>
+    <Container>
+      <StatusBar />
+      <FlatList
+        showsHorizontalScrollIndicator={false}
+        data={movies}
+        keyExtractor={item => item.key}
+        horizontal
+        contentContainerStyle={{
+          alignItems: 'center',
+        }}
+        renderItem={({ item }) => {
+          return (
+            <PosterContainer>
+              <Poster>
+                <PosterImage source={{ uri: item.posterPath }} />
+                <PosterTitle numberOfLines={1}>{item.originalTitle}</PosterTitle>
+                <Rating rating={item.voteAverage} />
+                <Genre genres={item.genres} />
+                <PosterDescription numberOfLines={5}>{item.description}</PosterDescription>
+              </Poster>
+            </PosterContainer>
+          );
+        }}
+      />
+    </Container>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+;
--- api.js
+++ api.js
@@ -2,15 +2,25 @@ import { API_KEY, GENRES } from "./.gitignore/config";
 
 const API_URL = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&sort_by=popularity.desc`
 
+const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYTlhNDI2MTViZmY0OWQxNDUzODNkNmY3NjJkZThiNCIsInN1YiI6IjY1MzE2ZTQ4NjkwNWZiMDBjOWYwNWE4MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.EXqXAJ28o-BV3gnDepJZVsK-lW5svuxcTHxI1DWk_DE'
+    }
+  };
+
 export const getMovies = async () => {
     try{
-        let response = await fetch(API_URL)
+        let response = await fetch('https://api.themoviedb.org/3/trending/movie/day?language=en-US', options)
         let json = await response.json();
+        // console.log(json);
         const movies = json.results.map(
             ({
                 id,
                 original_title,
                 poster_path,
+                backdrop_path,
                 vote_average,
                 overview,
                 release_date,
@@ -31,3 +41,6 @@ export const getMovies = async () => {
         console.log(error);
     }
 }
+
+
+
